d455138e14fbe0a9d5d0c3d0438ce5a0759b1b81
Charlotte Wegener <charlotteanne.wegener@gmail.com>
2022-02-16 16:09:10 +0100

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", rating: 5 }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London", rating: 4 }
+pizza_west =  { name: "Pizza West", address: "Checkpoint Charlie 2, Berlin", rating: 3 }
+spaghetti =  { name: "Spaghetti", address: "Sternstrasse 69, Düsseldorf", rating: 2 }
+nero =  { name: "Nero", address: "Marschallstrasse 29, Bonn", rating: 1 }
+
+[ dishoom, pizza_east ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
